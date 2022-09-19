f7326965c4b4a6feefd31a05c8c3211cfd431090
SantiagoCorvatta <santiagocorvatta@gmail.com>
2022-09-18 21:47:19 -0300

    PreEntrega1+Corvatta

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,9 +1,13 @@
+import { Badge } from 'react-bootstrap';
 import { TiShoppingCart } from 'react-icons/ti';
 
 const CartWidget = () => {
     return ( 
         <>
             <TiShoppingCart />
+            <span className='span'>
+                <h6>1</h6>              
+            </span> 
         </>
         
      );
